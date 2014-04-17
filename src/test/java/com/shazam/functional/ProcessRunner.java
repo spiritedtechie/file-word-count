4a38b9673de570f35d6142bec460d3dc4886feac
Vijay Patel <vijay@spiritedtechie.com>
2014-04-17 11:42:07 +0100

    extracted out process logic into own class.

---
+++ src/test/java/com/shazam/functional/ProcessRunner.java
@@ -0,0 +1,35 @@
+package com.shazam.functional;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class ProcessRunner {
+
+    public Process runWordCount(File testFile) throws Exception {
+        return runJarApplication(testFile);
+    }
+
+    public Process runLineCount(File testFile) throws Exception {
+        return runJarApplication(testFile, "-l");
+    }
+
+    private Process runJarApplication(File testFile, String... optionalExecArgs) throws Exception {
+        File jar = new File("target/word-count-0.0.1-SNAPSHOT.jar");
+        ArrayList<String> execArgsList =
+                buildExecArgs(jar.getCanonicalPath(), testFile.getCanonicalPath(), optionalExecArgs);
+        return Runtime.getRuntime().exec(execArgsList.toArray(new String[0]));
+    }
+
+    private ArrayList<String> buildExecArgs(String jarFilePath, String testFilePath, String... optionalExecArgs)
+            throws Exception {
+        ArrayList<String> execArgsList = new ArrayList<String>();
+        execArgsList.add("java");
+        execArgsList.add("-jar");
+        execArgsList.add(jarFilePath);
+        for (String optionalArg : optionalExecArgs) {
+            execArgsList.add(optionalArg);
+        }
+        execArgsList.add(testFilePath);
+        return execArgsList;
+    }
+}
--- src/test/java/com/shazam/functional/WordCountIT.java
+++ src/test/java/com/shazam/functional/WordCountIT.java
@@ -8,19 +8,26 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class WordCountIT {
+
+    private ProcessRunner pr;
+
+    @Before
+    public void setUp() {
+        pr = new ProcessRunner();
+    }
 
     @Test
     public void wordCount() throws Exception {
         // Given
         File testFile = aTestFile("word_count_integration_test.txt");
         // When
-        Process p = runWordCount(testFile);
+        Process p = pr.runWordCount(testFile);
         // Then
         assertCompleted(p);
         assertWordCount(9, extractCount(p.getInputStream()));
@@ -31,7 +38,7 @@ public class WordCountIT {
         // Given
         File testFile = aTestFile("word_count_integration_test.txt");
         // When
-        Process p = runLineCount(testFile);
+        Process p = pr.runLineCount(testFile);
         // Then
         assertCompleted(p);
         assertLineCount(4, extractCount(p.getInputStream()));
@@ -60,31 +67,4 @@ public class WordCountIT {
         }
     }
 
-    private Process runWordCount(File testFile) throws Exception {
-        return runJarApplication(testFile);
-    }
-
-    private Process runLineCount(File testFile) throws Exception {
-        return runJarApplication(testFile, "-l");
-    }
-
-    private Process runJarApplication(File testFile, String... optionalExecArgs) throws Exception {
-        File jar = new File("target/word-count-0.0.1-SNAPSHOT.jar");
-        ArrayList<String> execArgsList =
-                buildExecArgs(jar.getCanonicalPath(), testFile.getCanonicalPath(), optionalExecArgs);
-        return Runtime.getRuntime().exec(execArgsList.toArray(new String[0]));
-    }
-
-    private ArrayList<String> buildExecArgs(String jarFilePath, String testFilePath, String... optionalExecArgs)
-            throws Exception {
-        ArrayList<String> execArgsList = new ArrayList<String>();
-        execArgsList.add("java");
-        execArgsList.add("-jar");
-        execArgsList.add(jarFilePath);
-        for (String optionalArg : optionalExecArgs) {
-            execArgsList.add(optionalArg);
-        }
-        execArgsList.add(testFilePath);
-        return execArgsList;
-    }
 }
